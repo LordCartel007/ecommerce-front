5200d55863f012576a5e57363a54a48f17110f7e
LordCartel007 <cartellord77@gmail.com>
2025-01-13 17:14:47 +0100

    deleted some codes for some change

--- Ecommerce-admin/components/Spinner.js
+++
@@ -1,5 +0,0 @@
-// LOADING SPINNER
-import { SpinnerRoundFilled } from "spinners-react";
-export default function Spinner() {
-  return <SpinnerRoundFilled thickness={100} speed={100} color="#1E3A8A" />;
-}
