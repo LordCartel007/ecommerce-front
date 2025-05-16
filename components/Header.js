f9a9c703f28063c56b041d60d112a4c5fc07d688
LordCartel007 <cartellord77@gmail.com>
2025-05-16 18:57:44 +0100

    removed account link in header

--- components/Header.js
+++ components/Header.js
@@ -79,7 +79,7 @@ export default function Header() {
             <NavLink href={"/"}>Home</NavLink>
             <NavLink href={"/products"}>All Products</NavLink>
             <NavLink href={"/categories"}>Categories</NavLink>
-            <NavLink href={"/account"}>Account</NavLink>
+
             <NavLink href={"/cart"}>Cart ({cartProducts.length})</NavLink>
           </StyledNav>
           <NavButton onClick={() => setMobileNavActive((prev) => !prev)}>
